a61f63e9a86f990d59d0eb97e05f50c87758d2d7
Tiger Sun <ts3334@nyu.edu>
2020-10-14 13:08:43 -0400

    Update check_xpdf.py

--- xpdf_python/check_xpdf.py
+++ xpdf_python/check_xpdf.py
@@ -3,10 +3,14 @@ import sys
 
 <<<<<<< HEAD
 
+<<<<<<< HEAD
 if os.path.isfile('/usr/local/pdftotext'):
 =======
 if os.path.isfile('/usr/bin/pdftotext'):
 >>>>>>> parent of ee3dc92... save
+=======
+if os.path.isfile('/usr/local/bin/pdftotext'):
+>>>>>>> parent of ce84ac9... fixed pdftotext bug
 	pass
 else:
 	sys.exit("Did not detect correctly installed xpdf. Please follow install instructions at: https://github.com/ecatkins/xpdf_python.")
